c80bfc692563f923f517828586fdddc4846889d0
Rachit Jain <rachitjainjaipur@gmail.com>
2024-06-30 20:49:26 +0530

    first ques of contest

---
+++ 30June2024/Q1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int result = n * k - (k - 1);
+        cout << result << endl;
+    }
+    return 0;
+}
