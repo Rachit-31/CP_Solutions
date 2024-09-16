ebdf0b9229059c76bea4ecf313bffef60964c13a
Rachit Jain <rachitjainjaipur@gmail.com>
2024-09-16 21:48:05 +0530

    trailing zeros

--- CSES/TrailingZeros.cpp
+++ CSES/TrailingZeros.cpp
@@ -1,6 +1,20 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int main(){
-    cout<<"Hello world"<<endl;
+
+int trailingZeroes(int n) {
+    int count = 0;
+
+    // Count factors of 5
+    for (int i = 5; n / i > 0; i *= 5) {
+        count += n / i;
+    }
+
+    return count;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << trailingZeroes(n) << endl;
     return 0;
 }
