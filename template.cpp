22cbcb193ae295451282ee1138039be9db53c418
Rachit Jain <rachitjainjaipur@gmail.com>
2024-12-22 21:18:52 +0530

    .

--- template.cpp
+++ template.cpp
@@ -3,54 +3,53 @@ using namespace std;
 
 #define int long long        
 
-// Replaces 'endl' with '\n' for faster output (as 'endl' flushes output buffer)
+
 #define endl '\n'            
 
 #define fast_io() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL) 
 
-const int MOD = 1e9 + 7;      // For modular arithmetic
-const int INF = 1e18;         // A very large value used for initialization
+const int MOD = 1e9 + 7;      
+const int INF = 1e18;       
 
-// Function to perform modular addition
+
 int mod_add(int a, int b, int m = MOD) { 
     return ((a % m) + (b % m)) % m; 
 }
 
-// Function to perform modular subtraction
+
 int mod_sub(int a, int b, int m = MOD) { 
     return ((a % m) - (b % m) + m) % m; 
 }
 
-// Function to perform modular multiplication
+
 int mod_mul(int a, int b, int m = MOD) { 
     return ((a % m) * (b % m)) % m; 
 }
 
-// Debugging tools - active only when not in a competitive environment
+
 #ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;  // Macro to print debug information
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl; 
 #else
 #endif
 
-// Print functions for debugging various data types
+
 void _print(int t) {cerr << t;}
 void _print(string t) {cerr << t;}
 void _print(char t) {cerr << t;}
 void _print(double t) {cerr << t;}
 
-// Templates for printing containers (like vector, set, etc.) in debugging
 template <class T, class V> void _print(pair <T, V> p);
 template <class T> void _print(vector <T> v);
 template <class T> void _print(set <T> v);
 template <class T, class V> void _print(map <T, V> v);
 template <class T> void _print(multiset <T> v);
 
-// Template to print a pair
+
 template <class T, class V> void _print(pair <T, V> p) {
     cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";
 }
 
-// Template to print a vector
+
 template <class T> void _print(vector <T> v) {
     cerr << "[ "; 
     for (T i : v) {
@@ -59,7 +58,7 @@ template <class T> void _print(vector <T> v) {
     cerr << "]";
 }
 
-// Template to print a set
+
 template <class T> void _print(set <T> v) {
     cerr << "[ "; 
     for (T i : v) {
@@ -68,7 +67,7 @@ template <class T> void _print(set <T> v) {
     cerr << "]";
 }
 
-// Template to print a map
+
 template <class T, class V> void _print(map <T, V> v) {
     cerr << "[ "; 
     for (auto i : v) {
@@ -77,7 +76,7 @@ template <class T, class V> void _print(map <T, V> v) {
     cerr << "]";
 }
 
-// Template to print a multiset
+
 template <class T> void _print(multiset <T> v) {
     cerr << "[ "; 
     for (T i : v) {
