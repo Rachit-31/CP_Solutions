69684cad17d7bb06d37901d8642c9f8a6e72caf4
Rachit Jain <rachitjainjaipur@gmail.com>
2025-01-03 00:24:13 +0530

    simple

---
+++ Codeforces/EasyProblem.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void solve() {
+    int t;
+    cin >> t; 
+    while (t--) {
+        int n;
+        cin >> n; 
+        cout << n - 1 << '\n'; 
+    }
+}
+
+int main() {
+    solve();
+    return 0;
+}
