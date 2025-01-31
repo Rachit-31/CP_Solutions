d434ca4726a0f687732e030cc5d8b07452355f24
Rachit Jain <rachitjainjaipur@gmail.com>
2025-01-31 22:26:55 +0530

    solved using hint

---
+++ AtCoder/B_Count_Balls.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h> 
+using namespace std;     
+
+#define int long long        
+#define endl '\n'            
+
+#define fast_io() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL) 
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz(a) (int)a.size()
+#define pii pair<int, int>
+#define vi vector<int>
+#define mi map<int, int>
+#define umi unordered_map<int, int>
+#define si set<int>
+#define sc set<char>
+
+// functions
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define cf(i,s,e) for(int i=s;i<=e;i++)
+#define rf(i,e,s) for(int i=e-1;i>=s;i--)
+#define pb push_back
+#define in cin >>
+#define out cout <<
+
+const int MOD = 1e9 + 7;      
+const int INF = 1e18;  
+
+int gcd(int a,int b) { if (b==0) return a; return gcd(b, a%b); }
+int lcm(int a,int b) { return a/gcd(a,b)*b; }
+
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+void solve() {
+    int n, a, b;
+    in n;
+    in a;
+    in b;
+
+    int total = a+b;
+    int seg = n/total;
+    int rem = n%total;
+
+    out seg*a + min(rem, a);
+}
+
+int32_t main() {
+    fast_io();
+   
+    solve(); 
+    return 0; 
+}
