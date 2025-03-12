13396cf5f99d239dec46123fe3ccc521074ea858
Rachit Jain <rachitjainjaipur@gmail.com>
2025-03-12 12:45:39 +0530

    need revisionn. this ques is not done by me copy paste from outside

--- CSES/High_Score.cpp
+++ CSES/High_Score.cpp
@@ -1,172 +1,111 @@
-#include <bits/stdc++.h> 
-using namespace std;     
-
-#define int long long        
-#define nl <<'\n'           
-
-#define fast_io() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL) 
-#define sz(a) (int)a.size()
-#define pii pair<int, int>
-
-#define vi vector<int>
-#define vc vector<char>
-#define vs vector<string>
-#define vb vector<bool>
-
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(),v.rend()
-#define mi map<int, int>
-#define umi unordered_map<int, int>
-#define umc unordered_map<char, int>
-#define ums unordered_map<string, int>
-#define si set<int>
-#define sc set<char>
-
-// functions
-#define f(i,s,e) for(int i=s;i<e;i++)
-#define cf(i,s,e) for(int i=s;i<=e;i++)
-#define rf(i,e,s) for(int i=e;i>=s;i--)
-#define pb push_back
-#define in cin >>
-#define out cout <<
-
-const int MOD = 1e9 + 7;      
-const int INF = 1e18;  
-
-int gcd(int a,int b) { if (b==0) return a; return gcd(b, a%b); }
-int lcm(int a,int b) { return a/gcd(a,b)*b; }
-bool prime(int n){
-    if (n < 2)
-        return false;
-    for (int x = 2; x * x <= n; x++){
-        if (n % x == 0)
-            return false;
-    }
-    return true;
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define max3(a, b, c) max(max(a, b), c)
+#define max4(a, b, c, d) max(max(a, b), max(c, d))
+#define fr(i, n) for (ll i = 0; i < n; i++)
+ll gcd(ll a, ll b)
+{
+    return b == 0 ? a : gcd(b, a % b);
 }
-void ovi(vi &v){
-    f(i,0,v.size()){
-        cout<<v[i]<<" ";
-    }
-    cout<<endl;
-}
-void ovc(vc &v){
-    f(i,0,v.size()){
-        cout<<v[i]<<" ";
-    }
-    cout<<endl;
-}
-void ovs(vs &v){
-    f(i,0,v.size()){
-        cout<<v[i]<<" ";
-    }
-    cout<<endl;
-}
-
-void ivi(vi &v){
-    int n = v.size();
-    f(i,0,n){
-        in v[i];
-    }
-}
-void ivc(vc &v){
-    int n = v.size();
-    f(i,0,n){
-        in v[i];
-    }
-}
-void ivs(vs &v){
-    int n = v.size();
-    f(i,0,n){
-        in v[i];
-    }
-}
-
-void yes() { cout<<"YES\n"; }
-void no() { cout<<"NO\n"; }
-
-void solve() {
-    int n, m;
-    in n, in m;
-    
-    unordered_map<int, list<pii>>adj;
-    vector<vector<int>> rev_adj(n + 1); 
-
-    f(i,0,m){
-        int a, b, x;
-        in a, in b, in x;
-        adj[a].pb({b, x});
-        rev_adj[b].pb(a);
-    }
-
-    vi dist(n+1, -INF);
-
-    dist[1]=0;
-
-
-    f(i,1,n){
-        cf(u,1,n){
-            if(dist[u] == -INF) continue;
-
-            for(auto k: adj[u]){
-                int v = k.first;
-                int w = k.second;
-                if(dist[u]+w > dist[v]){
-                    dist[v] = dist[u]+w;
-                }
-            }
-        }
-    }
-
-
-    queue<int>q;
-    q.push(n);
-
-
-    vb vis(n+1, false);
-    while (!q.empty())
+vector<ll> vis(2501, 0);
+void dfs(ll node, ll parent, vector<ll> &reachable, vector<vector<ll>> &adjrev)
+{
+    if (vis[node] == 1)
     {
-        int top = q.front();
-        q.pop();
-        for(auto i: rev_adj[top]){
-            if(!vis[i]){
-                vis[i]=true;
-                q.push(i);
-            }
+        return;
+    }
+    else
+    {
+        reachable.push_back(node);
+        vis[node] = 1;
+        for (auto &i : adjrev[node])
+        {
+            if (i == parent)
+                continue;
+            dfs(i, node, reachable, adjrev);
         }
     }
-
-
-    f(i,1,n){
-        cf(u,1,n){
-            if(dist[u]==-INF) continue;
-
-            for(auto k: adj[u]){
-                int v = k.first;
-                int w = k.second;
-
-                if(dist[u] + w> dist[v] && vis[v]){
-                    out -1 nl;
-                    return;
-                }
-            }
-        }
-    }
-    
-    out dist[n] nl;
-
-
 }
+int main()
+{
+    fastio;
+    ll n, m;
+    cin >> n >> m;
+    vector<vector<pair<ll, ll>>> adj(n + 1);
+    vector<pair<pair<ll, ll>, ll>> edges;
+    for (ll i = 0; i < m; i++)
+    {
+        ll u, v, w;
+        cin >> u >> v >> w;
+        edges.push_back({{u, v}, -w});
+        adj[u].push_back({v, -w});
+    }
+    vector<ll> dist(n + 1, LLONG_MAX);
+    dist[1] = 0;
+    for (ll i = 1; i <= n; i++)
+    {
+        for (ll j = 0; j < m; j++)
+        {
+            ll u = edges[j].first.first;
+            ll v = edges[j].first.second;
+            ll w = edges[j].second;
+            if (dist[u] != LLONG_MAX)
+            {
+                dist[v] = min(dist[v], dist[u] + w);
+            }
+        }
+    }
+    // bool f = true;
+    ll x = dist[n];
+    set<ll> temp;
+    for (ll j = 0; j < m; j++)
+    {
+        ll u = edges[j].first.first;
+        ll v = edges[j].first.second;
+        ll w = edges[j].second;
+        if (dist[u] != LLONG_MAX)
+        {
+            ll x = dist[v];
+            dist[v] = min(dist[v], dist[u] + w);
+            if (x != dist[v])
+                temp.insert(v);
+        }
+    }
+    for (auto &i : edges)
+    {
+        swap(i.first.first, i.first.second);
+    }
+    vector<vector<ll>> adjrev(n + 1);
+    for (ll i = 0; i < m; i++)
+    {
+        ll u = edges[i].first.first;
+        ll v = edges[i].first.second;
+        adjrev[u].push_back(v);
+    }
+    vector<ll> reachable;
+    dfs(n, -1, reachable, adjrev);
+    bool f = true;
+    for (auto &i : reachable)
+    {
+        if (temp.find(i) != temp.end())
+        {
+            f = false;
+            break;
+        }
+    }
 
-int32_t main() {
-    fast_io();
-    // int t;  
-    // cin >> t;  
-    // while (t--) {
-    //     solve(); 
+    // if (x != dist[n])
+    // {
+    //     f = false;
     // }
-
-    solve();
-
-    return 0; 
+    if (f)
+        cout << -dist[n] << "\n";
+    else
+        cout << "-1\n";
 }
