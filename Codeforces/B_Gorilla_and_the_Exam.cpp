a76aee6b824ee2d9136852dac8f4a667ac609935
Rachit-31 <rachitjainjaipur@gmail.com>
2025-04-06 12:30:17 +0530

    .

---
+++ Codeforces/B_Gorilla_and_the_Exam.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h> 
+using namespace std;     
+
+#define int long long        
+#define nl <<'\n'           
+
+#define fast_io() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL) 
+#define sz(a) (int)a.size()
+#define pii pair<int, int>
+
+#define vi vector<int>
+#define vc vector<char>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define mi map<int, int>
+#define umi unordered_map<int, int>
+#define umc unordered_map<char, int>
+#define ums unordered_map<string, int>
+#define si set<int>
+#define sc set<char>
+
+// functions
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define cf(i,s,e) for(int i=s;i<=e;i++)
+#define rf(i,e,s) for(int i=e;i>=s;i--)
+#define pb push_back
+#define F first
+#define S second
+#define in cin >>
+#define out cout <<
+
+const int MOD = 1e9 + 7;      
+const int INF = 1e18;  
+
+int gcd(int a,int b) { if (b==0) return a; return gcd(b, a%b); }
+int lcm(int a,int b) { return a/gcd(a,b)*b; }
+bool prime(int n){
+    if (n < 2)
+        return false;
+    for (int x = 2; x * x <= n; x++){
+        if (n % x == 0)
+            return false;
+    }
+    return true;
+}
+void ovi(vi &v){
+    f(i,0,v.size()){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+void ovc(vc &v){
+    f(i,0,v.size()){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+void ovs(vs &v){
+    f(i,0,v.size()){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void ivi(vi &v){
+    int n = v.size();
+    f(i,0,n){
+        in v[i];
+    }
+}
+void ivc(vc &v){
+    int n = v.size();
+    f(i,0,n){
+        in v[i];
+    }
+}
+void ivs(vs &v){
+    int n = v.size();
+    f(i,0,n){
+        in v[i];
+    }
+}
+
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+void solve() {
+    int n, k;
+    in n >> k;
+    vi a(n);
+    ivi(a);
+
+    sort(all(a)); 
+    vi cnt = {1}; 
+
+    f(i,1,n){
+        if (a[i] == a[i-1]) {
+            cnt.back()++;
+        } else {
+            cnt.pb(1);
+        }
+    }
+
+    sort(all(cnt)); 
+    int m = sz(cnt); 
+
+    f(i,0,m-1){
+        if (cnt[i] > k) {
+            out m - i nl; 
+            return;
+        }
+        k -= cnt[i];
+    }
+
+    out 1 nl; 
+}
+
+int32_t main() {
+    fast_io();
+    int t;  
+    cin >> t;  
+    while (t--) {
+        solve(); 
+    }
+
+    return 0; 
+}
