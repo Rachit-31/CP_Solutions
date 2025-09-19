fba344834d651fb8a11d6de899143459562c48a5
Rachit-31 <rachitjainjaipur@gmail.com>
2025-09-19 22:12:35 +0530

    done

---
+++ Codeforces/E_Building_an_Aquarium.cpp
@@ -0,0 +1,147 @@
+#include <bits/stdc++.h> 
+using namespace std;     
+
+#define int long long        
+#define nl <<'\n'           
+
+#define fast_io() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL) 
+#define sz(a) (int)a.size()
+#define pii pair<int, int>
+
+#define vi vector<int>
+#define vc vector<char>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define mi map<int, int>
+#define umi unordered_map<int, int>
+#define umc unordered_map<char, int>
+#define ums unordered_map<string, int>
+#define si set<int>
+#define sc set<char>
+
+// functions
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define cf(i,s,e) for(int i=s;i<=e;i++)
+#define rf(i,e,s) for(int i=e;i>=s;i--)
+#define pb push_back
+#define bk back
+#define F first
+#define S second
+#define in cin >>
+#define out cout <<
+#define spc <<" "
+
+const int MOD = 1e9 + 7;      
+const int INF = 1e18;  
+
+int gcd(int a,int b) { if (b==0) return a; return gcd(b, a%b); }
+int lcm(int a,int b) { return a/gcd(a,b)*b; }
+bool prime(int n){
+    if (n < 2)
+        return false;
+    for (int x = 2; x * x <= n; x++){
+        if (n % x == 0)
+            return false;
+    }
+    return true;
+}
+void ovi(vi &v){
+    f(i,0,v.size()){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+void ovc(vc &v){
+    f(i,0,v.size()){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+void ovs(vs &v){
+    f(i,0,v.size()){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void ivi(vi &v){
+    int n = v.size();
+    f(i,0,n){
+        in v[i];
+    }
+}
+void ivc(vc &v){
+    int n = v.size();
+    f(i,0,n){
+        in v[i];
+    }
+}
+void ivs(vs &v){
+    int n = v.size();
+    f(i,0,n){
+        in v[i];
+    }
+}
+
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+int bs(int h, vi &a, int x){
+    int n= a.size();
+
+    int area=0;
+    f(i,0,n){
+        area += max(0LL, h - a[i]);
+        if(area >x){
+            break;
+        }
+    }
+
+    return area;
+}
+void solve() {
+    int n, x;
+    in n>>x;
+
+    
+
+    vi a(n);
+    ivi(a);
+    if(n==1){
+        out a[0]+x nl;
+        return;
+    }
+
+    int s=1, e=*max_element(all(a))+x;
+    int ans=-1;
+    while(s<=e){
+        int mid= s+(e-s)/2;
+        int area = bs(mid, a, x);
+
+        if(area > x){
+            e= mid-1;
+        } 
+        else{
+            ans = mid;
+            s= mid+1;
+        }
+    }
+
+    out ans nl;
+}
+
+int32_t main() {
+    fast_io();
+    int t;  
+    cin >> t;  
+    while (t--) {
+        solve(); 
+    }
+
+    // solve();
+
+    return 0; 
+}
