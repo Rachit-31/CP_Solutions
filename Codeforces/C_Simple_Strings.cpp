f2252155be18eee3df4b669ca24973914503909f
Rachit-31 <rachitjainjaipur@gmail.com>
2025-10-16 18:29:20 +0530

    simple ques

---
+++ Codeforces/C_Simple_Strings.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h> 
+using namespace std;     
+
+#define int long long        
+#define nl <<'\n'           
+
+#define fast_io() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL) 
+#define sz(a) (int)a.size()
+#define pii pair<int, int>
+
+#define vi vector<int>
+#define vc vector<char>
+#define vs vector<string>
+#define vb vector<bool>
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define mi map<int, int>
+#define umi unordered_map<int, int>
+#define umc unordered_map<char, int>
+#define ums unordered_map<string, int>
+#define si set<int>
+#define sc set<char>
+
+// functions
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define cf(i,s,e) for(int i=s;i<=e;i++)
+#define rf(i,e,s) for(int i=e;i>=s;i--)
+#define pb push_back
+#define bk back
+#define F first
+#define S second
+#define in cin >>
+#define out cout <<
+#define spc <<" "
+
+const int MOD = 1e9 + 7;      
+const int INF = 1e18;  
+
+int gcd(int a,int b) { if (b==0) return a; return gcd(b, a%b); }
+int lcm(int a,int b) { return a/gcd(a,b)*b; }
+bool prime(int n){
+    if (n < 2)
+        return false;
+    for (int x = 2; x * x <= n; x++){
+        if (n % x == 0)
+            return false;
+    }
+    return true;
+}
+void ovi(vi &v){
+    f(i,0,v.size()){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+void ovc(vc &v){
+    f(i,0,v.size()){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+void ovs(vs &v){
+    f(i,0,v.size()){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void ivi(vi &v){
+    int n = v.size();
+    f(i,0,n){
+        in v[i];
+    }
+}
+void ivc(vc &v){
+    int n = v.size();
+    f(i,0,n){
+        in v[i];
+    }
+}
+void ivs(vs &v){
+    int n = v.size();
+    f(i,0,n){
+        in v[i];
+    }
+}
+
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+void solve() {
+    string s;
+    in s;
+    int n= s.length();
+
+    if(n==1){
+        out s nl;
+        return;
+    }
+    int j=1;
+    
+    for(int i=0; i<=n-2; i++){
+        if(s[i] == s[i+1]){
+            int j=i+1;
+            
+            if(j+1 < n){
+                int k=j+1;
+                for(char c='a'; c<='z'; c++){
+                    if(c!=s[i] && c!=s[k]){
+                        s[j]=c;
+                        break;
+                    }
+                }
+            }
+            else{
+                if(s[j]=='z'){
+                    s[j]='a';
+                }
+                else{
+                    s[j]= s[j]+1;
+                }
+            }
+        }
+    }
+
+    out s nl;
+}
+
+int32_t main() {
+    fast_io();
+    solve();
+
+    return 0; 
+}
